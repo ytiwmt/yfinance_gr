4a000c5f9178393c3aebc9d093d81660f934ab84
ytiwmt <yt.iwmt@gmail.com>
2026-06-16 08:26:46 +0900

    Update check_growthradar.py

--- check_growthradar.py
+++ check_growthradar.py
@@ -325,6 +325,8 @@ def fetch(session, ticker):
             yearly_trend_factor = 1.0
         elif yearly_return > -0.2 and high_distance > -0.4:
             yearly_trend_factor = 0.5
+        elif yearly_return > -0.35 and high_distance > -0.55:
+    　　     yearly_trend_factor = 0.25
         else:
             yearly_trend_factor = 0.0
 
