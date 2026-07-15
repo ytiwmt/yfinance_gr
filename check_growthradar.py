0606607cb3a6d870425c3eb5cc54105eae69f988
ytiwmt <yt.iwmt@gmail.com>
2026-07-15 17:46:14 +0900

    Update check_growthradar.py

--- check_growthradar.py
+++ check_growthradar.py
@@ -371,13 +371,14 @@ def fetch(session, ticker):
             delta > -0.05 and
             not breakout and
             long_term_bonus >= 0.75 and
-            drawdown > -0.40
+            drawdown > -0.40 and
+            not second_wind_trigger
         )
 
         second_wind_trigger = (
             second_wind_watch and
             extension >= 1.8 and
-            delta > 0
+            delta > 0 
         )
 
         second_wind_quality = (
